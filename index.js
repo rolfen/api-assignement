83e718d0efcdf1a0a12aa4513f373f16590b0ac4
The Rolf <feedback@weknowtoomuch.com>
2016-02-24 22:48:26 +0000

    add Web API spec

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-/*
- * @return {Recipe}
- */
-var fetchById = function(recipeId) {
 
-}
-/*
- * @return {Recipes}
- */
-var fetchByCuisine = function(cuisine) {
-
-}
 
 var Recipe = function(properties) {
 
@@ -30,6 +19,54 @@ Recipe.prototype.update = function(properties) {
 var Recipes = function(arrayOfRecipes) {
 
 }
+/*
+ * @return {Recipes}
+ */
+Recipes.prototype.fetchBy = function(propertyName, propertyValue) {
+
+}
 Recipes.prototype.append = function(recipe) {
 
 }
+
+
+//=============================
+
+/* Web API
+
+// Retrieve
+fetchById
+	GET: id, properties
+	RETURNS: Recipe
+fetchByCuisine
+	GET: cuisine, properties
+	RETURNS: array of Recipe
+
+// Update
+rateRecipe
+	GET: id
+	POST: int score
+updateRecipe
+	GET: id
+	POST: Recipe
+
+// Create
+storeNewRecipe
+	POST: Recipe
+	RETURNS: id of new recipe
+
+// no Delete?
+
+Web API data format examples:
+
+Recipe:
+{
+	id: 12,
+	properties: {
+		title: "Yo",
+		fat_grams: 12
+	}
+}
+
+
+*/
