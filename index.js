2612d2043c3cba0cd39ca94862c47b1e40389bbc
The Rolf <feedback@weknowtoomuch.com>
2016-02-25 10:53:32 +0000

    add superagent for testing, and develop CSV parsing

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 'use strict';
 
+var fs = require('fs');
 var express = require('express');
 var Recipes = require('./lib/Recipes.js');
 var Recipe = require('./lib/Recipe.js');
 
-var goustoAPI = express();
+var parse = require('csv-parse');
 
-goustoAPI.get('/fetchById', function(req, res){
-	res.send({what:"yo!"});
+var api = express();
+
+// load data
+fs.readFile("data/input.csv", 'utf8', function(err, fileContents){
+	if(err) throw err;
+	parse(fileContents, function(err, parsedCsv){
+		/*
+		 * Convert a single parsed CSV record (row/line) to a JSON object
+		 * @param {Array} properties Array of property names
+		 * @param {Array} record An array of parsed CSV fields, will be mapped to the property names
+		 * @return {Objet} JSON representation of the record, with property values mapped to property names
+		 */
+		var toJson = function(properties, record) {
+			var recordObj = {};
+			record.forEach(function(val, index){
+				recordObj[properties[index]] = val;
+			})
+			return recordObj;
+		}
+
+		var data = parsedCsv.slice(1).map(function(val){
+			return toJson(parsedCsv[0], val);
+		});
+
+		startupServer(api, data);
+	})
 });
 
-goustoAPI.listen(80, function () {
-  console.log('Listening');
-});
+
+/*
+ * Starts up blank express app "api" with preloaded data "data"
+ */
+var startupServer = function(api, data) {
+	api.get('/printAll', function(req, res){
+		res.send({what:data});
+	});
+
+	api.get('/sayHello', function(req, res){
+		res.send({what:data});
+	});
+
+	api.get('/fetchById', function(req, res){
+		res.send({what:"yo!"});
+	});
+
+	api.listen(80, function () {
+	  console.log('Listening');
+	});
+}
+
 
 //=============================
 
